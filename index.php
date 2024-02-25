128789248de91b8166382aa5480ccbd804e3f2a1
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-02-25 18:19:14 -0300

    feat ~ configure routes

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 <?php 
 
-phpinfo();
+$url = (!empty(filter_input(INPUT_GET, 'url', FILTER_DEFAULT)) ? filter_input(INPUT_GET, 'url', FILTER_DEFAULT) : "home");
+//var_dump($url);
+
+
+$url = array_filter(explode('/', $url));
+//print_r($url);
+
+$arquivo = 'src/pages/'.$url[0].'.php';
+//var_dump("<br>".$arquivo);
+
+if (is_file($arquivo)) {
+	include $arquivo;
+}else{
+	include 'src/pages/not-found.html';
+}
+
+
 ?>
