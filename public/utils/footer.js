e96517b089ab9babc31c17ac314ce551d27cd2a7
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-02-26 19:54:10 -0300

    feat(utils) ~ add new footer

---
+++ public/utils/footer.js
@@ -0,0 +1,44 @@
+document.write(`
+	<style>
+		@font-face {
+		  font-family: 'Protest Riot';
+		  src: url("../../public/font/Protest_Riot/ProtestRiot-Regular.ttf");
+		}
+		footer.footer_class, hr{
+		  font-family: 'Protest Riot', sans-serif;
+		  bottom: 0;
+		  left: 0;
+		  height: 40px;
+		  position: fixed;
+		  width: 100%;
+		  text-align: center;
+		  display: flex;
+		  flex-direction: row;
+		  aling-items: center;
+		  justify-content: center;
+		  /*justify-content: space-between;*/
+		}
+		hr{
+			display: flex;
+			aling-items: center;
+			justify-content: center;
+			margin-left: 10rem;
+			width: 75%;
+			border-top: 1.5px solid black;
+		}
+	</style>
+	<div class="footer_class">
+		<hr>
+		<footer class="footer_class">
+		    <p>&copy; 2024 BlogsBlogs</p>
+		    <!--<a href="">Lexi</a>-->
+		    <!--<a href="">Lexi</a>-->
+		    <!--<a href="">Lexi</a>-->
+		    <!--<a href="">Lexi</a>-->
+		</footer>
+
+	</div>
+	
+	
+
+	`);
