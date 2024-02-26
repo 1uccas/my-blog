e50e8f433b3c15b3b2b687537800b177d6bf6ff7
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-02-26 15:27:31 -0300

    refactor(pages) ~ transform post db in function ~> queryMyPosts()

--- src/pages/home.php
+++ src/pages/home.php
@@ -1,19 +1,20 @@
 <?php
-require_once 'src/conf/db.php';
+function queryMyPosts(){
+	require_once 'src/conf/db.php';
 
-$mysql = new DatabaseConnection();
-$link = $mysql->getLink();
+	$mysql = new DatabaseConnection();
+	$link = $mysql->getLink();
 
-$sql = "SELECT * FROM my_posts";
-$result = $link->query($sql);
+	$sql = "SELECT * FROM my_posts";
+	$result = $link->query($sql);
 
-if ($result->num_rows > 0) {
-	while ($row = $result->fetch_assoc()) {
-		print_r($row);
-		echo "<br>";
-	}	
+	if ($result->num_rows > 0) {
+		while ($row = $result->fetch_assoc()) {
+			print_r($row);
+			echo "<br>";
+		}	
+	}
 }
-
 ?>
 <head>
 	<meta charset="utf-8">
