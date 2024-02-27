bcb3f47bedcc0d63b2784e757723b377d8c81ada
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-02-27 18:24:21 -0300

    refactor(utils) ~ refactor header.js

--- public/utils/header.js
+++ public/utils/header.js
@@ -11,35 +11,35 @@ document.write(`
 			font-family: 'Protest Riot', sans-serif;
 		}
 		#navbar_header{
+			background: linear-gradient(to right, #128e21, #08c549);
+			padding-top: 10px;
+			padding-bottom: 10px;
 			box-shadow: 5px 5px 8px gray;
 		}
 		#txtHeader{
-			font-size: 30px;
+			color: white;
+			font-size: 35px; 
+			margin-left: 3%;
+		}
+		#txtHeader:hover{
+			cursor: pointer;
 		}
 		.navbar-nav a{
 			color: white;
 		}
+		img{
+			height: auto;
+			margin: 5px;
+			width: 30px;
+		}
 	</style>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-	<nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark" id="navbar_header">
+	<nav class="navbar_class" data-bs-theme="light" id="navbar_header">
 	  <div class="container-fluid">
-	    <a class="navbar-brand" href="home"><label id="txtHeader">Blogs~Blogs</label></a>
+	    <a class="navbar-brand" href="home"><label id="txtHeader">Flip~<img src="../../public/img/owl.png">~Flops</label></a>
 	    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 	      <span class="navbar-toggler-icon"></span>
 	    </button>
-	    <div class="collapse navbar-collapse" id="navbarNav">
-	      <ul class="navbar-nav">
-	        <li class="nav-item">
-	          <a class="nav-link" href="#" >Home</a>
-	        </li>
-	        <li class="nav-item">
-	          <a class="nav-link" href="#">About</a>
-	        </li>
-	        <li class="nav-item">
-	          <a class="nav-link" href="#">Posts</a>
-	        </li>
-	      </ul>
-	    </div>
 	  </div>
 	</nav>
 	<br>
