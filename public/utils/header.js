4375a8d1078390221f49206eba69a5605f7f0b1b
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-02-27 18:30:51 -0300

    feat(utils) ~  new color in header

--- public/utils/header.js
+++ public/utils/header.js
@@ -11,7 +11,7 @@ document.write(`
 			font-family: 'Protest Riot', sans-serif;
 		}
 		#navbar_header{
-			background: linear-gradient(to right, #128e21, #08c549);
+			background: linear-gradient(to right, #0a6114, #27b639);
 			padding-top: 10px;
 			padding-bottom: 10px;
 		}
