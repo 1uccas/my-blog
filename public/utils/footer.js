ab349a882e419bc3ea7a817d95859f2483dc9acc
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-02-28 15:24:12 -0300

    refactor(utils) ~ making the footer responsive

--- public/utils/footer.js
+++ public/utils/footer.js
@@ -4,32 +4,43 @@ document.write(`
 		  font-family: 'Protest Riot';
 		  src: url("../../public/font/Protest_Riot/ProtestRiot-Regular.ttf");
 		}
-		footer.footer_class{
-		  font-family: 'Protest Riot', sans-serif;
-		  bottom: 0;
-		  left: 0;
-		  height: 40px;
-		  position: relative;
-		  width: 100%;
-		  text-align: center;
-		  display: flex;
-		  flex-direction: row;
-		  aling-items: center;
-		  justify-content: space-evenly;
-		}
-		hr{
-			display: flex;
-			aling-items: center;
-			justify-content: center;
-			margin-left: 10rem;
-			width: 75%;
-			border-top: 1.5px solid black;
-		}
+		.footer_class {
+	        width: 100%;
+	    }
+
+	    .footer_class_footer {
+	        text-align: center;
+	        padding: 20px;
+	    }
+
+	    .contact-me{
+	    	margin-left: 50px;
+	    }
+
+	    .footer_class_footer p {
+	        margin: 0;
+	    }
+
+	    .footer_class_footer a {
+	        margin: 0 10px;
+	        text-decoration: none;
+	    }
+	    hr{
+	    	margin-left: 150px;
+	    	margin-right: 150px;
+	    }
+
 	</style>
 	<div class="footer_class">
 		<hr>
-		<footer class="footer_class">
-		    <p>&copy; 2024 Flip ~ Flops - Todos os direitos reservados </p>
+		<footer class="footer_class_footer">
+		    <p><a href=""><img src="../../public/img/owl.png"></a>&copy; 2024 Flip ~ Flops - Todos os direitos reservados </p>
+		    <div class="contact-me">
+		    	<a href="" target="_blank">Sobre</a>
+		    	<a href="https://www.instagram.com/_1uccass/" target="_blank">Instagram</a>
+		    	<a href="https://github.com/Luccxx/" target="_blank">Github</a>
+		    </div>
+	    	
 		</footer>
 	</div>
 	`);
