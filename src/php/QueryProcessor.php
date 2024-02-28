63efac293baf6415c4f4f50c833d787d63f15a4e
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-02-28 17:12:36 -0300

    feat(pages) ~ add function takeData and format all datas in db

--- src/php/QueryProcessor.php
+++ src/php/QueryProcessor.php
@@ -1,21 +1,44 @@
 <?php 
 function queryHomeMyPosts(){
-	require_once 'src/conf/db.php';
+    function takeData($allData){
+    	$arrayMounth = ["", "Janeiro", "Fevereiro", "Março", "Abril", "Maio", 
+    	"Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
 
-	$mysql = new DatabaseConnection();
-	$link = $mysql->getLink();
+        $data = date("n", strtotime($allData));
+        $year = date("Y", strtotime($allData));
+        $time = date("H:i", strtotime($allData));
 
-	$sql = "SELECT * FROM my_posts";
-	$result = $link->query($sql);
+        return array(
+	        "month" => $arrayMounth[$data],
+	        "year" => $year,
+	        "time" => $time
+	    );
+    }
 
-	if ($result->num_rows > 0) {
-		while ($row = $result->fetch_assoc()) {
-			echo "
-				<h3>$row[title]</h3>
-				<p>$row[post]</p>
-				";
-				
-		}	
-	}
+    require_once 'src/conf/db.php';
+
+    $mysql = new DatabaseConnection();
+    $link = $mysql->getLink();
+
+    $sql = "SELECT * FROM my_posts";
+    $result = $link->query($sql);
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $data = $row['current_data']; 
+            
+            $formatted_data = takeData($data);
+            $month = $formatted_data['month'];
+            $year = $formatted_data['year'];
+            $time = $formatted_data['time'];
+
+            echo "
+            <div class='div_only_post' title='Post in ~ $time'>
+                <h2>$year ~ $month</h2> 
+                <h3>{$row['title']}</h3> 
+                <p>{$row['post']}</p>
+            </div>";
+        }   
+    }
 }
 ?>
