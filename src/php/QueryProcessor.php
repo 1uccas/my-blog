10df6a248be079443c8df1bb12886701bdffc947
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-02-29 21:07:30 -0300

    refactor(php) ~ refactor function FormattedUrl

--- src/php/QueryProcessor.php
+++ src/php/QueryProcessor.php
@@ -11,9 +11,41 @@ function queryHomeMyPosts(){
 
         return $result;
     }
+
+    function FormattedUrl($queryUrl){
+        $caracters = array(
+            'á' => 'a',
+            'à' => 'a',
+            'â' => 'a',
+            'ã' => 'a',
+            'é' => 'e',
+            'ê' => 'e',
+            'í' => 'i',
+            'ó' => 'o',
+            'ô' => 'o',
+            'õ' => 'o',
+            'ú' => 'u',
+            'ç' => 'c',
+            ',' => '',
+            "'" => '',
+            "!" => '',
+            "?" => ''
+        );
+
+        $urlPost = $queryUrl;
+        $explodeUrl = explode(" ", $urlPost); //separa url
+        $newUrl = array_slice($explodeUrl, 3); //pega da terceira posição até o final do array 
+        $removeCaracters = array_map(function($word) use ($caracters) {
+            return strtr($word, $caracters);
+        }, $newUrl);
+        $implodeUrl = implode("-", $removeCaracters); // junta todos os array com "-" 
+
+        return $implodeUrl; 
+    }
+
     function takeData($allData){
-    	$arrayMounth = ["", "Janeiro", "Fevereiro", "Março", "Abril", "Maio", 
-    	"Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+        $arrayMounth = ["", "Janeiro", "Fevereiro", "Março", "Abril", "Maio", 
+        "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
 
         $data = date("n", strtotime($allData)); //Month 
         $year = date("Y", strtotime($allData));
@@ -21,50 +53,20 @@ function queryHomeMyPosts(){
         $day = date("d", strtotime($allData));
 
         return array(
-	        "month" => $arrayMounth[$data],
-	        "year" => $year,
-	        "time" => $time,
-	        "day" => $day
-	    );
+            "month" => $arrayMounth[$data],
+            "year" => $year,
+            "time" => $time,
+            "day" => $day
+        );
     }
 
     $result = returnDataPost();
 
-    $caracters = array(
-        'á' => 'a',
-        'à' => 'a',
-        'â' => 'a',
-        'ã' => 'a',
-        'é' => 'e',
-        'ê' => 'e',
-        'í' => 'i',
-        'ó' => 'o',
-        'ô' => 'o',
-        'õ' => 'o',
-        'ú' => 'u',
-        'ç' => 'c',
-        ',' => '',
-        "'" => '',
-        "!" => '',
-        "?" => ''
-    );
-
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
             $data = $row['current_data'];
-
-            $urlPost = $row['title'];
-            $explodeUrl = explode(" ", $urlPost); // separa a URL em palavras
-            $newUrl = array_slice($explodeUrl, 3); // pega da terceira posição até o final do array 
-
-            // Remove a acentuação de cada palavra no array
-            $removeCaracters = array_map(function($word) use ($caracters) {
-                return strtr($word, $caracters);
-            }, $newUrl);
-
-            $implodeUrl = implode("-", $removeCaracters); // junta todas as palavras do array com "-"
-
-            print_r($implodeUrl);
+            $url = $row['title'];
+            $url = FormattedUrl($url);
 
             $formatted_data = takeData($data);
             $month = $formatted_data['month'];
@@ -74,16 +76,16 @@ function queryHomeMyPosts(){
 
             echo "
             <div class='class_time_post'>
-            	<label>$year - $month</label>
-        	</div>
-        	<div class='div_master_only_post'>
-        		<a href=''>
-        			<div class='div_only_post' title='Post in ~ $time'>
-		                <label class='label_title_post'>{$row['title']}</label> 
-		                <label class='label_fullData_post'>$day $month $year · $time</label>
-		            </div>
-        		</a>
-        	</div>
+                <label>$year - $month</label>
+            </div>
+            <div class='div_master_only_post'>
+                <a href='post/{$url}'>
+                    <div class='div_only_post' title='Post in ~ $time'>
+                        <label class='label_title_post'>{$row['title']}</label> 
+                        <label class='label_fullData_post'>$day $month $year · $time</label>
+                    </div>
+                </a>
+            </div>
             ";
         }   
     }
