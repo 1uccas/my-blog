a7d0b7c5de7b4269a6683ff1518f07004ca76034
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-03-06 17:33:23 -0300

    feat(php) ~ check if the id is being passed through the url

--- src/pages/post.php
+++ src/pages/post.php
@@ -1,6 +1,13 @@
 <?php 
 include_once 'src/php/QueryProcessor/QueryProcessor.php';
-$id = $url[1];
+if (isset($url[1])) {
+	$id = $url[1];
+	QueryPostID($id);
+} else {
+	include 'src/pages/not-found.html';
+	die("Status ~> 404");
+}
 
-QueryPostID($id);
+
+
 ?>
