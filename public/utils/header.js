a6df3f86a92716f8a1c1db2238c71f0902bb016c
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-03-06 21:14:17 -0300

    style(utils) ~ add background black and font color white

--- public/utils/header.js
+++ public/utils/header.js
@@ -11,7 +11,7 @@ document.write(`
 			font-family: 'Protest Riot', sans-serif;
 		}
 		#navbar_header{
-			background: #db8005;
+			background: #0d1117;
 			padding-top: 10px;
 			padding-bottom: 10px;
 		}
@@ -32,15 +32,16 @@ document.write(`
 			width: 30px;
 		}
 	</style>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-	<nav class="navbar_class" data-bs-theme="light" id="navbar_header">
-	  <div class="container-fluid">
-	    <a class="navbar-brand" href="home"><label id="txtHeader">Flip~<img src="../../public/img/owl.png">~Flops</label></a>
-	    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-	      <span class="navbar-toggler-icon"></span>
-	    </button>
-	  </div>
-	</nav>
-	<br>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+	<main>
+		<nav class="navbar_class" data-bs-theme="light" id="navbar_header">
+		  <div class="container-fluid">
+		    <a class="navbar-brand" href="home"><label id="txtHeader">Flip~<img src="../../public/img/owl_white_color.png">~Flops</label></a>
+		    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+		      <span class="navbar-toggler-icon"></span>
+		    </button>
+		  </div>
+		</nav>
+	</main>
+	
+
 	`);
