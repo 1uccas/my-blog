369df010432310f0c798a1f8d67652b8ef7d6295
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-03-08 16:10:40 -0300

    build ~ composer require erusev/parsedown

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -60,6 +60,16 @@ class ComposerStaticInit92f22c6250a0bd409f5e837231010ea0
         ),
     );
 
+    public static $prefixesPsr0 = array (
+        'P' => 
+        array (
+            'Parsedown' => 
+            array (
+                0 => __DIR__ . '/..' . '/erusev/parsedown',
+            ),
+        ),
+    );
+
     public static $classMap = array (
         'Attribute' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Attribute.php',
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
@@ -74,6 +84,7 @@ class ComposerStaticInit92f22c6250a0bd409f5e837231010ea0
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit92f22c6250a0bd409f5e837231010ea0::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit92f22c6250a0bd409f5e837231010ea0::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit92f22c6250a0bd409f5e837231010ea0::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit92f22c6250a0bd409f5e837231010ea0::$classMap;
 
         }, null, ClassLoader::class);
