4dc9ae29831610366e262f0f51ffeaf99a7dce7f
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-03-30 17:35:01 -0300

    feat(header) ~ remove button in container-fluid

--- public/utils/header.js
+++ public/utils/header.js
@@ -44,9 +44,6 @@ document.write(`
 		<nav class="navbar_class" data-bs-theme="light" id="navbar_header">
 		  <div class="container-fluid">
 		    <a class="navbar-brand" href="/../home"><label id="txtHeader">Flip~<img src="../../public/img/owl_white_color.png">~Flops</label></a>
-		    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-		      <span class="navbar-toggler-icon"></span>
-		    </button>
 		  </div>
 		</nav>
 	</main>
